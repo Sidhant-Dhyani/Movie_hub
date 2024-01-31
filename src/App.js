55c4875b972ca8aea5738e2a31e5489b4c34f3f3
sidhant dhyani <siddhantydhyani99@gmail.com>
2024-02-01 00:47:03 +0530

    project done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import MoviePage from './components/moviepage/MoviePage'
+import MovieContainer from './components/moviecontainer/MovieContainer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<MovieContainer />} />
+        <Route path='/movie/:id' element={<MoviePage />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
